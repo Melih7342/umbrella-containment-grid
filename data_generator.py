04ba3667abacb68acc88242cefd2c0ed224c9a74
Melih7342 <melih.gueldogdu@stud.fh-campuswien.ac.at>
2026-09-01 00:38:34 +0200

    began with generator script

---
+++ data_generator.py
@@ -0,0 +1,10 @@
+def data_generator():
+    # {"sensor_id": "T-04", "type": "MOTION", "sector": "B4-TYRANT-LAB", "value": "HIGH", "timestamp": "..."}
+    data = {}
+    while True:
+        for i in range()
+
+
+def generate_motion_data(dict, sensor_num):
+    sectors = {"B4-TYRANT-LAB": "MOTION", "Chemical Experiment Room": "MOTION", "Power Room", "Visual Data Room", "West Area", "East Area", "Waste Disposal Plant"}
+
